100831ab1840e81accf171ebb896e9f7c728fd71
Liam <lbonnage@gmail.com>
2019-09-20 20:59:55 -0500

    Initial commit, connection to MongoDB created

---
+++ server.py
@@ -0,0 +1,25 @@
+import pymongo
+from flask import Flask
+
+
+mongo = pymongo.MongoClient('mongodb+srv://lbonnage:Mongodb@hackrice9-ricefloodprediction-zqrfo.gcp.mongodb.net/test?retryWrites=true&w=majority', maxPoolSize=50, connect=False)
+
+db = pymongo.database.Database(mongo, 'test')
+col = pymongo.collection.Collection(db, 'node')
+
+print(col.find_one())
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+	return "Hello world!"
+
+# @app.route('/click')
+# def handle_click(click):
+# 	coll.add(click)
+
+
+if __name__ == '__main__':
+	app.run()
+
